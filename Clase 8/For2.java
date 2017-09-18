55d5f8817f2d380d5a8e75843a79459eb91a3e89
Isaac Harari <ISAAC@Isaacs-iMac.local>
2017-09-18 12:48:13 -0500

    for de coordenadas

---
+++ Clase 8/For2.java
@@ -0,0 +1,13 @@
+
+public class For2{
+  public static void main (String[] args){
+
+    for (int i = 1;i<=5 ;i++ ) {
+
+      for (int j = 1;j<=4 ;j++ ) {
+
+        System.out.printf("(%d,%d) ",i,j);
+      }
+    }
+  }
+}
